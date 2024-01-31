db8148eff813139059f0c447c4976a958277e938
supekarsuraj <surajsupekar2323@gmail.com>
2024-02-01 00:20:57 +0530

    backend chnages

--- backend/config/dbconnection.js
+++ backend/config/dbconnection.js
@@ -1,9 +1,9 @@
 const mysql2=require('mysql2');
 var con=mysql2.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"todolist"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 con.connect((err)=>{
     if(err) throw err;
